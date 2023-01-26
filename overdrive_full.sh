495e6239baa498d7cb85f2123999bb64d7a77ccc
bender <jonasmakai@gmail.com>
2023-01-25 19:15:12 -0500

    updates

--- overdrive_full.sh
+++ overdrive_full.sh
@@ -10,24 +10,24 @@ bash overdrivedelete.sh
 
 # extract chapters, specify folder holding the unpackaged overdrive mp3's
 source config.env
-python3.10 extract_overdrive_chapters.py $annex
+python3 extract_overdrive_chapters.py $annex
 
 # clean metadata
-python3.10 metadatatoxml.py
+python3 metadatatoxml.py
 
-python3.10 xmlparse.py
+python3 xmlparse.py
 
 # turn chapters into ms
-python3.10 chapter_ms.py
+python3 chapter_ms.py
 
 # format durations
-python3.10 durations.py
+python3 durations.py
 
 # export labeled audio and archive original folder 
 # FIX: MAKE SURE IT CAN ADD TO EXISTING AUTHOR FOLDER
-python3.10 00_exportchapters.py
+python3 00_exportchapters.py
 
 # tag the exported audio with proper metadata
-python3.10 final_metadata_add.py
+python3 final_metadata_add.py
 
 #manually add to Plex
